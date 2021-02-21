100be89e01d673ec9f9fa1d4e57643056eaaebd8
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-21 11:05:11 -0600

    chained methods

--- chaining.js
+++ chaining.js
@@ -3,12 +3,14 @@ const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
 const numEl = document.getElementById("integers")
 numEl.innerHTML += "<h1>Integers</h1>";
+numEl.innerHTML += "<hr>";
 
 
 // Sort the numbers in descending order (10, 9, 8, 7, etc).
 numEl.innerHTML += "<h3>Acending</h3>";
 const sortNumbers = integers.sort( (a, b) => a - b );
 numEl.innerHTML += `${sortNumbers}`;
+numEl.innerHTML += "<hr>";
 
 
 
@@ -16,13 +18,15 @@ numEl.innerHTML += `${sortNumbers}`;
 numEl.innerHTML += "<h3>Numbers Under 19</h3>";
 const removeNumbers = integers.splice(0, 10);
 numEl.innerHTML += `${removeNumbers}`;
+numEl.innerHTML += "<hr>";
 
 
 
 // Multiply each remaining number by 1.5 and then subtract 1.
 numEl.innerHTML += "<h3>Multiply 1.5 & Subtract 1</h3>";
-const multiplyNumbers = integers.map(integer => integer * 1.5 - 1)
+const multiplyNumbers = integers.map(integer => integer * 1.5 - 1);
 numEl.innerHTML += `${multiplyNumbers}`;
+numEl.innerHTML += "<hr>";
 
 
 
@@ -30,7 +34,13 @@ numEl.innerHTML += `${multiplyNumbers}`;
 numEl.innerHTML += "<h3>Sum Of Remaining Numbers</h3>";
 const sum = integers.reduce((total, amount) => total + amount); 
 numEl.innerHTML += `${sum}`;
+numEl.innerHTML += "<hr>";
 
+
+numEl.innerHTML += "<h3>All The Things Ending In SUM</h3>";
+const chainMethods = integers.sort( (a, b) => a - b ).splice(0, 10).map(integer => integer * 1.5 - 1).reduce((total, amount) => total + amount); 
+numEl.innerHTML += `${chainMethods}`;
+numEl.innerHTML += "<hr>";
 
 
 
